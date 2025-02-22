f48f705c7ee42b7e39adf34eaf980b49d5baad94
Lorenzo Lu <lorenzo.lu@mailbox.org>
2025-02-22 15:22:08 +0100

    Add momentum to optimizer.

--- npgrad/optim.py
+++ npgrad/optim.py
@@ -2,20 +2,18 @@ __all__ = ["Optimizer", "SGD"]
 
 from typing import Iterable
 
+from numpy.typing import NDArray
+
 from npgrad._array import Array
 
 
 class Optimizer:
     def __init__(self, params: Iterable[Array], lr: float) -> None:
         self._params = tuple(params)
-        self.lr = lr
-
-    @property
-    def params(self) -> tuple[Array, ...]:
-        return self._params
+        self._lr = lr
 
     def zero_grad(self) -> None:
-        for p in self.params:
+        for p in self._params:
             p.grad = None
 
     def step(self) -> None:
@@ -23,9 +21,66 @@ class Optimizer:
 
 
 class SGD(Optimizer):
+    _momentum_buf: list[NDArray | None]
+
+    def __init__(
+        self,
+        params: Iterable[Array],
+        lr: float = 1e-3,
+        momentum: float = 0,
+        dampening: float = 0,
+        weight_decay: float = 0,
+        nesterov: bool = False,
+    ) -> None:
+        super().__init__(params, lr)
+        self._momentum = momentum
+        self._dampening = dampening
+        self._weight_decay = weight_decay
+        self._nesterov = nesterov
+        self._momentum_buf = [None] * len(self._params) if momentum else []
+
     def step(self) -> None:
-        for p in self.params:
-            if p.requires_grad:
-                if p.grad is None:
-                    raise RuntimeError("parameter gradient is None")
-                p.data -= self.lr * p.grad
+        _sgd(
+            self._params,
+            self._momentum_buf,
+            self._lr,
+            self._momentum,
+            self._dampening,
+            self._weight_decay,
+            self._nesterov,
+        )
+
+
+def _sgd(
+    params: Iterable[Array],
+    momentum_buf: list[NDArray | None],
+    lr: float,
+    momentum: float,
+    dampening: float,
+    weight_decay: float,
+    nesterov: bool,
+) -> None:
+    for i, p in enumerate(params):
+        if p.grad is not None:
+            d_p = p.grad
+
+            if weight_decay:
+                # do not modify d_p in-place
+                d_p = d_p + weight_decay * p.data
+
+            if momentum:
+                buf = momentum_buf[i]
+
+                if buf is None:
+                    buf = momentum_buf[i] = d_p.copy() if d_p is p.grad else d_p
+                else:
+                    buf *= momentum
+                    buf += (1 - dampening) * d_p
+
+                if nesterov:
+                    # do not modify d_p in-place
+                    d_p = d_p + momentum * buf
+                else:
+                    d_p = buf
+
+            p.data -= lr * d_p
